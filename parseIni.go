a48a1113e214ba94d97e41add3ad14b7879cae72
wen.gong <gwalker@126.com>
2019-01-29 12:44:20 +0800

    add blog

--- parseIni.go
+++ parseIni.go
@@ -1,6 +1,6 @@
 /**********************************************
 ** @Des: This file ...
-** @Author: gongwen
+** @Author: gongwen [https://www.gwalker.cn]
 ** @Date:   2018-10-03 15:42:43
 ** @Last Modified by:   gongwen
 ** @Last Modified time: 2019-01-29 11:49:17
